34070e572302abd89f858546c0b2e1fd9a7131d9
th050111 <th050111@naver.com>
2021-12-17 11:10:54 +0900

    canvas1.0

--- main.js
+++ main.js
@@ -31,44 +31,96 @@ const screen = {
     width: 960,
     height: 540,
   },
-  position: {
-    x: 250,
-    y: 200,
-  },
+  position: {},
 };
 
-coverCtx.fillStyle = "#cc0000"; // ìƒ‰ìƒ
-coverCtx.fillRect(350, 200, 1050, 630); // ë„í˜•
+function init() {
+  screen.position = {
+    x: window.innerWidth / 2 - screen.size.width / 2,
+    y: window.innerHeight / 2 - screen.size.height / 2,
+  };
+}
 
-function draw() {
+init();
+
+function handleGame() {
+  cv.style.top = canvasList.game.position.y + "px";
+  cv.style.left = canvasList.game.position.x + "px";
+  ctx.fillStyle = "blue";
+  ctx.fillRect(50, 50, 200, 200);
+}
+
+function handleCover() {
   //background-cover
   coverCtx.fillStyle = "black"; // ìƒ‰ìƒ
   coverCtx.fillRect(
-    canvasList.cover.size.width,
-    canvasList.cover.size.height,
     canvasList.cover.position.x,
-    canvasList.cover.position.y
+    canvasList.cover.position.y,
+    canvasList.cover.size.width,
+    canvasList.cover.size.height
   ); // ë„í˜•
   //screen
-  coverCtx.fillStyle = "#cc0000"; // ìƒ‰ìƒ
-  coverCtx.fillRect(
+  coverCtx.clearRect(
     screen.position.x,
     screen.position.y,
     screen.size.width,
     screen.size.height
-  ); // ë„í˜•
-
-  cv.style.top = canvasList.game.position.x + "px";
-  cv.style.left = canvasList.game.position.y + "px";
+  );
 }
 
-draw();
+//애니메이션 관리
+function animate() {
+  ctx.clearRect(
+    0,
+    0,
+    canvasList.game.size.width,
+    canvasList.game.size.height
+  );
+  coverCtx.clearRect(
+    0,
+    0,
+    canvasList.cover.size.width,
+    canvasList.cover.size.height
+  );
 
-// window.addEventListener("resize", () => {
-//   canvasList.cover.size = {
-//     width: window.innerWidth,
-//     height: window.innerHeight,
-//   };
-//   coverCv.width = canvasList.cover.size.width;
-//   coverCv.height = canvasList.cover.size.height
-// });
+  handleCover();
+  handleGame();
+
+  requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener("resize", () => {
+  screen.position = {
+    x: window.innerWidth / 2 - screen.size.width / 2,
+    y: window.innerHeight / 2 - screen.size.height / 2,
+  };
+  console.log(screen);
+});
+
+const howManyCanvasMove = 10;
+
+window.addEventListener("keydown", (key) => {
+  let directionX = 0;
+  let directionY = 0;
+  switch (key.code) {
+    case "ArrowUp":
+      directionY = -1;
+      break;
+    case "ArrowDown":
+      directionY = 1;
+      break;
+    case "ArrowLeft":
+      directionX = -1;
+      break;
+    case "ArrowRight":
+      directionX = 1;
+      break;
+    default:
+      directionX = directionY = 0;
+  }
+  canvasList.game.position.x -=
+    howManyCanvasMove * directionX;
+  canvasList.game.position.y -=
+    howManyCanvasMove * directionY;
+});
